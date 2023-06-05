c20562e4b69601d16a16cee80ab31c8a6544462d
Breno Melo Porcel <melobreno89@gmail.com>
2023-06-05 14:57:13 -0300

    creating project

---
+++ project/src/main/java/com/br/project/library/project/model/Livro.java
@@ -0,0 +1,11 @@
+package com.br.project.library.project.model;
+
+public abstract class Livro {
+    private String titulo;
+    private String autor;
+    private int quantidadePaginas;
+    private Double valor;
+    private String linguagem;
+    private String nomeTradutor;
+    private String editora;
+}
