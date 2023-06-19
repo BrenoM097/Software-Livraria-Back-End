d44f3f4542e0116a5628a35c79a7744bb73224ce
Breno Melo Porcel <melobreno89@gmail.com>
2023-06-19 11:09:36 -0300

    Configuração CORS
    configuração para o CORS

--- project/src/main/java/com/br/project/library/project/model/Livro.java
+++ project/src/main/java/com/br/project/library/project/model/Livro.java
@@ -1,6 +1,5 @@
 package com.br.project.library.project.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
